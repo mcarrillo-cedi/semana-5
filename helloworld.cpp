1a60bd401d4000c0e487db6f47dc7681c59bb30e
Mario Carrillo <mcarrillo@cedi.edu.mx>
2024-02-14 13:08:20 -0600

    Agrega x

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int main()
 {
 
-
     cout << "Hello world!" << endl;
     cout << "bye world" << endl;
+    int x;
+    cin >> x;
+    cout << "el numero es " << x << endl;
+
+    // este es un comentario
     return 0;
 }
